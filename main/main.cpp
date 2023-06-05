a13effbd879eeb58c06de418d283a092990872c0
Aggelos Vogiatzis <aggelos100vo@gmail.com>
2023-06-05 18:49:19 +0300

    Sample code

--- main/main.cpp
+++ main/main.cpp
@@ -16,8 +16,10 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "BlkBx";
+
 
 extern "C"
 {
@@ -26,7 +28,7 @@ extern "C"
 
 void app_main(void)
 {
-
+    // Init logo print
     printf("▄▄▄▄    ██▓     ██ ▄█▀ ▄▄▄▄   ▒██   ██▒\n");
     printf("▓█████▄ ▓██▒     ██▄█▒ ▓█████▄ ▒▒ █ █ ▒░\n");
     printf("▒██▒ ▄██▒██░    ▓███▄░ ▒██▒ ▄██░░  █   ░\n");
@@ -38,9 +40,18 @@ void app_main(void)
     printf("░          ░  ░░  ░    ░       ░    ░\n");
     printf("░                      ░\n");
 
+
+    // --Main program--
+
+    gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT); // Set GPIO direction
+
     while (1)
     {
-        ESP_LOGI(TAG, "Hello");
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "On");
+        gpio_set_level(GPIO_NUM_15, 1); // Turn led ON
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Off");
+        gpio_set_level(GPIO_NUM_15, 0); // Turn led OFF
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
